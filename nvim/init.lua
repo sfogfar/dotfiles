374a2b314f14404ff2df798f13b909b9a0d8b958
Sam Cooper <003.sjc@gmail.com>
2022-09-10 15:35:13 +0100

    more sane directional control of resizing

--- nvim/init.lua
+++ nvim/init.lua
@@ -70,10 +70,10 @@ keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)        -- TODO: Resolve conflict with MacOS workspace nav shortcut
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)       -- TODO: Resolve conflict with MacOS workspace nav shortcut
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize -2<CR>", opts)        -- TODO: Resolve conflict with MacOS workspace nav shortcut
+keymap("n", "<C-Left>", ":vertical resize +2<CR>", opts)       -- TODO: Resolve conflict with MacOS workspace nav shortcut
 
 -- navigate buffers
 keymap("n", "<left>", ":bnext<CR>", opts)
