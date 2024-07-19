b165c2d9617e6ff5bf04ff617d1aba053fa933c3
Sam Cooper <003.sjc@gmail.com>
2024-07-19 15:39:29 +0100

    updates from new work mpb14

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,5 @@
 set -g fish_greeting
 starship init fish | source
-
 set fzf_fd_opts --hidden --exclude=.git
-
 fish_vi_key_bindings
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
+fzf_configure_bindings
